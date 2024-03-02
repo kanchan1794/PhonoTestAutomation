e01fccf22041b2b7fac6b5991cbfa0472d0e075d
kanchan <kanchan1794@gmail.com>
2024-03-02 12:12:49 +0530

    my Commit

--- src/test/java/pageobjectclasses/PHPlanMenuPage.java
+++ src/test/java/pageobjectclasses/PHPlanMenuPage.java
@@ -10,7 +10,7 @@ import java.time.Duration;
 public class PHPlanMenuPage extends PHBrowserConfig {
 
 
-    private WebDriver driver = null;
+    private WebDriver driver;
 
     public PHPlanMenuPage(WebDriver driver) {
         //super(driver);
