f4ef6e1df31aeaa43cde44bfd544f78861df103f
kanchan <kanchan1794@gmail.com>
2024-03-02 19:22:31 +0530

    my Commit

--- src/test/java/pageobjectclasses/PHBrowserConfig.java
+++ src/test/java/pageobjectclasses/PHBrowserConfig.java
@@ -10,7 +10,7 @@ public class PHBrowserConfig {
 
     public static ThreadLocal<WebDriver> driverThread = ThreadLocal.withInitial(() -> {
        //WebDriver driver = new ChromeDriver();
-        //System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
         WebDriver driver = null;
         try {
             ChromeOptions options = new ChromeOptions();
@@ -23,7 +23,7 @@ public class PHBrowserConfig {
 
 
             //System.setProperty("webdriver.chrome.driver", "F:\\Eclipse-WorkSpace\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
-            System.setProperty("webdriver.chrome.driver", "/ec2-user/chromedriver");
+           // System.setProperty("webdriver.chrome.driver", "/ec2-user/chromedriver");
 
             driver = new ChromeDriver(options);
             driver.manage().window().maximize(); // Maximize the browser window
